8703c1f5ae80a590775dfd078a20b9c51dc3cfdc
mohmmadnoorjebreen <mohammadnoormjk1998@gmail.com>
2021-09-26 13:41:35 +0300

    added login page

---
+++ src/components/Log.js
@@ -0,0 +1,25 @@
+
+import './log.css'
+export default function Login() {
+
+    return (
+        <>
+            <form>
+                <h1>Login</h1>
+                <div class="inset">
+                    <p>
+                        <label for="User">User Name</label>
+                        <input type="text" name="text" id="User" placeholder="User Name"/>
+                    </p>
+                    <p>
+                        <label for="password">PASSWORD</label>
+                        <input type="password" name="password" id="password" placeholder="Password"/>
+                    </p>
+                </div>
+                <p class="p-container">
+                    <input type="submit" name="go" id="go" value="Log in"/>
+                </p>
+            </form>
+        </>
+    )
+}
