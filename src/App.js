5d1bfa05c43e505dd01fba7d0558bcf4af1936d2
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-09-26 15:51:48 +0300

    adding sideBar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import SidebarWithHeader from './components/SideBar';
+import Footer from './components/Footer'
+
+import Home from './pages/Home';
+
+import Profile from "./pages/Profile"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+
+        <SidebarWithHeader>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+          <Footer></Footer>
+        </SidebarWithHeader>
+
+
+      </Router>
+
+
+
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import { Link } from "@chakra-ui/layout";
+import BasicStatistics from "../components/Statistics";
+function Home(){
+
+    return (
+      <>
+      
+         <p>
+         Welcome Back Haya !
+         
+         </p>
+         <BasicStatistics></BasicStatistics>
+   
+       </>
+    );
+}
+
+export default Home;
