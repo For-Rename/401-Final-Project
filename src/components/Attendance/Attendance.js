ae34a1ca7deb68dd951e6bd326adccfe1227a194
Ali Rahhal <arahal81@gmail.com>
2021-09-27 13:01:14 +0300

    linked the component with sidebar

--- src/components/Attendance/Attendance.js
+++ src/components/Attendance/Attendance.js
@@ -1,13 +1,21 @@
-import { useState } from 'react';
-import Table from './Table';
-
+import { useState } from "react";
+import Table from "./Table";
+import {
+  Box,
+  chakra,
+  SimpleGrid,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+} from "@chakra-ui/react";
 function CDate() {
   // const dt = null;
-  // const [cdate,setDate] = useState(dt); 
+  // const [cdate,setDate] = useState(dt);
   // const handelDate = () =>{
   //   let dt = new Date().toLocaleString();
   //   setDate(dt);
-
 
   const [Attendance, setAttendance] = useState([]);
 
@@ -20,18 +28,18 @@ function CDate() {
       name: "sara",
       checkin: dt,
       checkout: dt,
-      id:Attendance.length+1,
+      id: Attendance.length + 1,
     };
-    setAttendance([...Attendance,value]);
+    setAttendance([...Attendance, value]);
     console.log(Attendance);
   }
   return (
-    <>
+    <Box maxW="7xl" mx={"auto"} pt={10} px={{ base: 2, sm: 12, md: 17 }}>
       {/* <h3>{Attendance.map(Attendance => <div>{Attendance.name}</div>)}</h3> */}
       <button onClick={submitHandler}>Check-in/Check-out</button>
-      <Table Attendance={Attendance} setAttendance={setAttendance}/>
-    </>
-  )
+      <Table Attendance={Attendance} setAttendance={setAttendance} />
+    </Box>
+  );
 }
 
 export default CDate;
