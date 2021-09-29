eed00ff5035c42b35e6b2c94e48ead7fe5c25520
Ali Rahhal <arahal81@gmail.com>
2021-09-29 11:53:52 +0300

    get user info

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -5,43 +5,58 @@ import axios from "axios";
 const tokenUrl = "http://localhost:8000/api/token/";
 const AuthContext = createContext();
 export function useAuth() {
-    const auth = useContext(AuthContext);
-    if (!auth) throw new Error("You forgot AuthProvider!");
-    return auth;
+  const auth = useContext(AuthContext);
+  if (!auth) throw new Error("You forgot AuthProvider!");
+  return auth;
 }
 export function AuthProvider(props) {
-    const [state, setState] = useState({
-        tokens: null,
-        user: null,
-        login,
-        logout,
-    });
-    async function login(username, password) {
-        const obj = {
-            "username": username,
-            "password": password
-        };
-        const response = await axios.post(tokenUrl, obj);
+  const [state, setState] = useState({
+    tokens: null,
+    user: null,
+    login,
+    logout,
+  });
+  async function login(username, password) {
+    const obj = {
+      username: username,
+      password: password,
+    };
+    const response = await axios.post(tokenUrl, obj);
 
-        const decodedAccess = jwt.decode(response.data.access);
-        const newState = {
-            tokens: response.data,
-            user: {
-                username: decodedAccess.username,
-                email: decodedAccess.email,
-                id: decodedAccess.user_id,
-            },
-        };
-        setState((prevState) => ({ ...prevState, ...newState }));
-    }
-    function logout() {
-        const newState = {
-            tokens: null,
-            user: null,
-        };
-        setState((prevState) => ({ ...prevState, ...newState }));
-    }
-    return (
-        <AuthContext.Provider value={state}>{props.children}</AuthContext.Provider>
+    const decodedAccess = jwt.decode(response.data.access);
+
+    const userinfo = await axios.get(
+      `http://localhost:8000/api/hrboost/userinfo/${decodedAccess.user_id}/`,
+      makeConfig(response.data.access)
     );
+
+    localStorage.setItem("userinfo", JSON.stringify(userinfo.data[0]));
+
+    const newState = {
+      tokens: response.data,
+      user: {
+        username: decodedAccess.username,
+        email: decodedAccess.email,
+        id: decodedAccess.user_id,
+      },
+    };
+    setState((prevState) => ({ ...prevState, ...newState }));
+  }
+  function logout() {
+    const newState = {
+      tokens: null,
+      user: null,
+    };
+    setState((prevState) => ({ ...prevState, ...newState }));
+  }
+  return (
+    <AuthContext.Provider value={state}>{props.children}</AuthContext.Provider>
+  );
+}
+function makeConfig(token) {
+  return {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  };
 }
