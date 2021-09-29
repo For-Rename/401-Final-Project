837bed7c501862189bcb5aa9b5b8714a6e9d78d7
Ali Rahhal <arahal81@gmail.com>
2021-09-29 10:47:02 +0300

    add satistics from database

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -5,43 +5,85 @@ import axios from "axios";
 const tokenUrl = "http://localhost:8000/api/token/";
 const AuthContext = createContext();
 export function useAuth() {
-    const auth = useContext(AuthContext);
-    if (!auth) throw new Error("You forgot AuthProvider!");
-    return auth;
+  const auth = useContext(AuthContext);
+  if (!auth) throw new Error("You forgot AuthProvider!");
+  return auth;
+}
+async function leavesHandler(tokens, user) {
+  if (!tokens) {
+    return;
+  }
+  const response = await axios.get(
+    "http://localhost:8000/api/hrboost/vacationsuser/" + user + "/",
+    makeConfig(tokens)
+  );
+  console.log(response.data);
+  const annuallyvac = response.data.filter(
+    (item) => item.vacation_type > "annually"
+  );
+  const hourlyvac = response.data.filter(
+    (item) => item.vacation_type > "hourly"
+  );
+  console.log("annuallyvac", annuallyvac);
+  console.log("hourlyvac", hourlyvac);
+
+  let sum_days_vac = annuallyvac.map((item) => {
+    var date1 = new Date(item.start_date);
+    var date2 = new Date(item.end_date);
+
+    var Difference_In_Time = date2.getTime() - date1.getTime();
+    return Difference_In_Time / (1000 * 3600 * 24);
+  });
+  console.log(sum_days_vac);
+  return sum_days_vac;
 }
 export function AuthProvider(props) {
-    const [state, setState] = useState({
-        tokens: null,
-        user: null,
-        login,
-        logout,
-    });
-    async function login(username, password) {
-        const obj = {
-            "username": username,
-            "password": password
-        };
-        const response = await axios.post(tokenUrl, obj);
+  const [state, setState] = useState({
+    tokens: null,
+    user: null,
+    sum_days_vac: 0,
+    login,
+    logout,
+  });
+  async function login(username, password) {
+    const obj = {
+      username: username,
+      password: password,
+    };
+    const response = await axios.post(tokenUrl, obj);
 
-        const decodedAccess = jwt.decode(response.data.access);
-        const newState = {
-            tokens: response.data,
-            user: {
-                username: decodedAccess.username,
-                email: decodedAccess.email,
-                id: decodedAccess.user_id,
-            },
-        };
-        setState((prevState) => ({ ...prevState, ...newState }));
-    }
-    function logout() {
-        const newState = {
-            tokens: null,
-            user: null,
-        };
-        setState((prevState) => ({ ...prevState, ...newState }));
-    }
-    return (
-        <AuthContext.Provider value={state}>{props.children}</AuthContext.Provider>
-    );
+    const decodedAccess = jwt.decode(response.data.access);
+    // const sum_days_vac = leavesHandler(
+    //   response.data.access,
+    //   decodedAccess.user_id
+    // );
+    const newState = {
+      tokens: response.data,
+      // sum_days_vac: sum_days_vac,
+      user: {
+        username: decodedAccess.username,
+        email: decodedAccess.email,
+        id: decodedAccess.user_id,
+      },
+    };
+
+    setState((prevState) => ({ ...prevState, ...newState }));
+  }
+  function logout() {
+    const newState = {
+      tokens: null,
+      user: null,
+    };
+    setState((prevState) => ({ ...prevState, ...newState }));
+  }
+  return (
+    <AuthContext.Provider value={state}>{props.children}</AuthContext.Provider>
+  );
+}
+function makeConfig(token) {
+  return {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  };
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,26 @@
-
 import Statistics from "../components/Statistics";
 
-import Blog  from "../components/Blog"
+import Blog from "../components/Blog";
 
-import BlogForm from '../components/BlogForm';
+import BlogForm from "../components/BlogForm";
 
-
-function Home(props){
- 
-
-    return (
-      <>
-      
-        
-         <Statistics remaining={props.remaining} performanceHandler={props.performanceHandler} perforPercentage={props.perforPercentage}performance={performance}></Statistics>
-         <Blog blog = {props.blog}showingModel={props.showingModel}></Blog>
-         <BlogForm  blogInfoHandler={props.blogInfoHandler} model={props.model} hidingModel={props.hidingModel}></BlogForm>
-   
-       </>
-    );
-
+function Home(props) {
+  return (
+    <>
+      <Statistics
+        // remainingdays={props.remainingdays}
+        performanceHandler={props.performanceHandler}
+        perforPercentage={props.perforPercentage}
+        performance={props.performance}
+      ></Statistics>
+      <Blog blog={props.blog} showingModel={props.showingModel}></Blog>
+      <BlogForm
+        blogInfoHandler={props.blogInfoHandler}
+        model={props.model}
+        hidingModel={props.hidingModel}
+      ></BlogForm>
+    </>
+  );
 }
 
 export default Home;
